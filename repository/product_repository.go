0c0bd85a8bbd582b23ffff9898036d9029d9249a
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-11 19:05:57 -0300

    feat: add CRUD operations to product repository

---
+++ repository/product_repository.go
@@ -0,0 +1,128 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"go-api/model"
+)
+
+type ProductRepository struct {
+	connection *sql.DB
+}
+
+func NewProductRepository(connection *sql.DB) ProductRepository {
+	return ProductRepository{
+		connection: connection,
+	}
+}
+
+func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
+	query := "SELECT id, product_name, price FROM product"
+
+	rows, err := pr.connection.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return []model.Product{}, err
+	}
+
+	var productList []model.Product
+	var productObj model.Product
+
+	for rows.Next() {
+		err := rows.Scan(
+			&productObj.ID,
+			&productObj.Name,
+			&productObj.Price)
+
+		if err != nil {
+			fmt.Println(err)
+			return []model.Product{}, err
+		}
+
+		productList = append(productList, productObj)
+	}
+
+	rows.Close()
+
+	return productList, nil
+}
+
+func (pr *ProductRepository) CreateProduct(product model.Product) (int, error) {
+	var id int
+	query, err := pr.connection.Prepare("INSERT INTO product (product_name, price) VALUES ($1, $2) RETURNING id")
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	err = query.QueryRow(product.Name, product.Price).Scan(&id)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	query.Close()
+
+	return id, nil
+}
+
+func (pr *ProductRepository) GetProductById(product_id int) (*model.Product, error) {
+	query, err := pr.connection.Prepare("SELECT * FROM product WHERE id = $1")
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var product model.Product
+
+	err = query.QueryRow(product_id).Scan(
+		&product.ID,
+		&product.Name,
+		&product.Price,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		fmt.Println(err)
+		return nil, err
+	}
+
+	query.Close()
+	return &product, nil
+}
+
+func (pr *ProductRepository) DeleteProductById(product_id int) error {
+	query, err := pr.connection.Prepare("DELETE FROM product WHERE id = $1")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	query.QueryRow(product_id)
+
+	query.Close()
+	return nil
+}
+
+func (pr *ProductRepository) UpdateProduct(product_id int, update_product model.Product) (int, error) {
+	var id int
+	query, err := pr.connection.Prepare("UPDATE product SET product_name = $1, price = $2 WHERE id = $3 RETURNING id")
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	err = query.QueryRow(update_product.Name, update_product.Price, product_id).Scan(&id)
+
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	query.Close()
+	return id, nil
+}
